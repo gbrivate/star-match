cc82bbc56d0e87ef138353a3249165e80197d10e
ivate <Fugir2018>
2019-04-08 16:25:46 -0300

    first game with react, from Samer Buna course.

--- src/renderers/dom.js
+++ src/renderers/dom.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from 'components/App';
 
 import '../styles/index.scss';
+import App from '../components/App';
 
 ReactDOM.hydrate(
   <App initialData={window.__R_DATA.initialData} />,
