1cf7ea110f9d6bbef0e6d9d7d5c208403bda35f7
ivate <Fugir2018>
2019-04-11 12:42:59 -0300

    Updates

---
+++ precache-manifest.406379fd568adb33c99685735fdbe3ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "509e2b7a620f2dd0faed",
+    "url": "/start-match/static/js/runtime~main.d9f3a508.js"
+  },
+  {
+    "revision": "2c5edd49fca687883f3b",
+    "url": "/start-match/static/js/main.5ce1e5cb.chunk.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/start-match/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "2c5edd49fca687883f3b",
+    "url": "/start-match/static/css/main.a9e64fb1.chunk.css"
+  },
+  {
+    "revision": "0e1858f2a9579c15d7d71f1204bf114f",
+    "url": "/start-match/index.html"
+  }
+];
--- server/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const env = process.env;
-module.exports = {
-  port: env.PORT || 4242,
-  host: env.HOST || 'localhost',
-  isDev: env.NODE_ENV !== 'production',
-  isBrowser: typeof window !== 'undefined'
-};
